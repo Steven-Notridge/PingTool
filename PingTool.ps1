ce2290354f9b15a24dbbdcdf4dfd0ef33668468a
Steven Notridge <89914092+Steven-Notridge@users.noreply.github.com>
2022-07-03 23:45:44 +0100

    Updated Methods
    Removed the Notification function. Trying to get that to work whilst the script ran as a background job, didn't work. Method changed to monitor log file consistently. Created Schedule Script for easy use.

--- PingTool.ps1
+++ PingTool.ps1
@@ -3,6 +3,8 @@
 # We test the connection to the outside world.
 # If the connection is down, it records the time and date, also proving the connection to the router was alive. If it's not, it records that too. In my case, it kept restarting randomly too.
 # If the connection is alive, but the ping is high, it will record that information as well as where it is going to.
+# We no longer run this with the Notifications. Though it's a cool idea, it's not practical and isn't really fitting for a 'workplace' - You would have some logging and monitoring instead.
+# Then again, this was a personal project, instead of being a 'workplace' project, however we may as well investigate what else can happen.
 
 # UK Date formatting.
 $Date = Get-Date -format "dd/MM HH:mm"
@@ -16,39 +18,18 @@ $TesterRouter = Test-Connection 192.168.0.1 -Quiet
 
 # Second Section
 # Not to be confused with the $TesterBoolean, this will reply with actual timings.
-$ResponseTime = Test-Connection $Alive -Count 10
+$ResponseTime = Test-Connection $Alive -Count 5
 # We format the timings, and grab the Maximum ping, and the Average of all 5 pings.
 $RT = $ResponseTime.ResponseTime | Measure-Object -Maximum -Average
 # Cleaner formatting for use in the script.
 $RTAverage = $RT.Average
 $RTMax = $RT.Maximum
 
-# Create a function to create Windows Notifications. Mostly because it'll be neater and I want some form of unintrusive notification.
-
-function Write-Notification {
-
-    # Create a parameter that allows us to type a message that we want to use later on.
-    param (
-        [Parameter(Mandatory = $true)]
-        [string]
-        $Message
-    )
-    # Setting up the notification
-    [reflection.assembly]::loadwithpartialname('System.Windows.Forms')
-    [reflection.assembly]::loadwithpartialname('System.Drawing')
-    $notify = New-Object system.windows.forms.notifyicon
-    $notify.Icon = [System.Drawing.SystemIcons]::Information
-    $notify.Visible = $true
-    $notify.ShowBalloonTip(100,'WARNING',$Message,[system.windows.forms.tooltipicon]::None)
-    # Unregister-Event clears the previous job, this is to stop overlapping if ran several times. It's necessary otherwise the next line won't execute correctly.
-    Unregister-Event -SourceIdentifier BalloonClicked_event -ErrorAction SilentlyContinue
-    # Executing code if the Notification is clicked.
-    Register-ObjectEvent $notify BalloonTipClicked BalloonClicked_event -Action {Start-Process -FilePath "./Log.txt"} | Out-Null
-}
+Set-Location -Path "C:\Sysadmin\GitCommits\PingTool\"
 
 # If the connection fails...
 if ($TesterBoolean -eq $False) {
-    Write-Host 'No Connection! The time is:' $Time
+    Write-Host 'No Connection! The time is:' $Date
     # Create a variable for the message to avoid issues with new lines.
     $msg = "[$Date] No Connection to $Alive!"
     Write-Output $msg | Out-File -FilePath .\Log.txt -Append
@@ -57,14 +38,12 @@ if ($TesterBoolean -eq $False) {
         Write-Host 'Connection to the Router has also failed, did it restart again?'
         Write-Output "Connection to the Router has also failed, did it restart again?" | Out-File -FilePath .\Log.txt -Append
         Write-Output "`n" | Out-File -FilePath .\Log.txt -Append
-        Write-Notification -Message "The connection to the router is down."
     }
     # If the connection to the router is alive...
     else {
         $msg = "The connection to the Router was successful."
         Write-Output $msg | Out-File -FilePath .\Log.txt -Append
         Write-Output "`n" | Out-File -FilePath .\Log.txt -Append
-        Write-Notification -Message "The internet appears to be down. The router is connected."
     }
 }
 
@@ -78,6 +57,5 @@ else {
         # Retrieve the Average and Maximum response times.
         $msg = "[$Date] Ping is reaching high values. The average response time was $RTAverage ms to $Alive, whilst the maximum response time was $RTMax ms."
         Write-Output $msg | Out-File -FilePath .\Log.txt -Append
-        Write-Notification -Message "Connection ping has reached high values. $RTAverage ms."
     }
 }
---
+++ Schedule-PingTool.ps1
@@ -0,0 +1,55 @@
+# Schedule Job for PingTool, and start displaying as log file. I don't want to have to type any commands to start and monitor everything.
+
+$JobAlive = Get-ScheduledJob -Name "PingTool" -ErrorAction SilentlyContinue
+$FilePath = "C:\Sysadmin\GitCommits\PingTool\PingToolv2.ps1"
+$TaskTrigger = (New-JobTrigger -Once -At "07/03/2022 0am" -RepetitionInterval (New-TimeSpan -Minutes 1) -RepetitionDuration ([TimeSpan]::MaxValue))
+
+If ([bool]($JobAlive) -eq $False){
+    Write-Host "`r"
+    Write-Host "No Job found. Creation will follow." -ForegroundColor Yellow
+    Register-ScheduledJob -Name 'PingTool' -FilePath $FilePath -Trigger $TaskTrigger | Out-Null
+    If ([bool](Get-ScheduledJob -Name "PingTool") -eq $True){
+        $Job = Get-ScheduledJob -Name "PingTool"
+        Write-Host "`r"
+        Start-Sleep -Seconds 1
+        Write-Host "Job created, with ID of" $Job.Id -ForegroundColor Green
+        Write-Host "`r"
+        Write-Host "Force starting Job for first run." -ForegroundColor Yellow
+        (Get-ScheduledJob -Name "PingTool").StartJob() | Out-Null
+        Start-Sleep -Seconds 1
+        Write-Host "`r"
+        If ($Job.Enabled -eq $True){
+            Write-Host "Status is now"$Job.Enabled -ForegroundColor Green
+        }
+        Else {
+            Write-Host "Status is now"$Job.Enabled -ForegroundColor Red
+            Write-Host "See below for Error."
+            Write-Output $Error[0]
+            Break
+        }
+    }
+}
+
+elseif ([bool]($JobAlive) -eq $True) {
+    Write-Host "`r"
+    Write-Host "Job Detected, checking current state." -ForegroundColor Green
+    $Job = Get-ScheduledJob -Name "PingTool"
+    if ($Job.Enabled -eq $false){
+        Write-Host "Job is not enabled. Starting Job." -ForegroundColor Yellow
+        Enable-ScheduledJob -Name "PingTool" | Out-Null
+        Start-Sleep -Seconds 1
+        Write-Host "Status is"$Job.Enabled -ForegroundColor Green
+    }
+    else {
+        Write-Host "`r"
+        Write-Host "Job is already enabled." -ForegroundColor Green
+    }
+}
+
+else {
+    Write-Host "Nothing required. Script continuing."
+}
+Write-Host "`r"
+Write-Host "Monitoring Log file..."
+Write-Host "`r"
+Get-Content -Path "C:\Sysadmin\GitCommits\PingTool\Log.txt" -Tail 4 -Wait
