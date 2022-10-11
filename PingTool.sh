1ca1e907c7849e90513f79e5336dc6a2431b5773
Steven Notridge <89914092+Steven-Notridge@users.noreply.github.com>
2022-10-12 00:29:57 +0100

    Update PingTool.sh
    Updated for use with cronjob instead of keeping the terminal open.

--- PingTool.sh
+++ PingTool.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# This line will ensure cronjob checks here for the log file.
+cd "$(dirname "$0")";
 FILE=log.txt
 TARGET=1.1.1.1
 ROUTER=192.168.0.1
@@ -10,31 +12,27 @@ if [[ -f "$FILE" ]]; then
         echo "$FILE doesn't exist. Creating file..."
         touch $FILE
         fi
+        
+    DATE=$(date '+%d/%m/%Y %H:%M')
 
-while true;
-        do
-        DATE=$(date '+%d/%m/%Y %H:%M')
-
-        # Ping once, see if the connection is alive.
-        ping -c 1 $TARGET &> /dev/null
-        # If ping does not connect, as this returns a boolean value. 0 means false. 1 means true.
-        if [[ $? -ne 0 ]]; then
-                echo >> $FILE
-                echo "["$DATE"]" "Ping failure to "$TARGET >> $FILE
-                ping -c 1 $ROUTER &> /dev/null
-                if [[ $? -ne 0 ]]; then
-                    echo >> $FILE
-                    echo "Ping failure to "$ROUTER "Did it restart?" >> $FILE
-                        else
-                        echo "Connection to Router is alive. Internet appears to be down." >> $FILE
-                    fi
-        fi
-        RESULTS=$(ping -c 5 1.1.1.1 | tail -1)
-        RESULTSAVG=$(echo $RESULTS | awk -F '/' '{print $5}' | cut -d "." -f 1 )
-        RESULTSMAX=$(echo $RESULTS | awk -F '/' '{print $6}' | cut -d "." -f 1 )
-        if [[ $RESULTSMAX -gt 75 ]]; then
+    # Ping once, see if the connection is alive.
+    ping -c 1 $TARGET &> /dev/null
+    # If ping does not connect, as this returns a boolean value. 0 means false. 1 means true.
+    if [[ $? -ne 0 ]]; then
             echo >> $FILE
-            echo "["$DATE"]" "Ping is reaching high values. The average response time was" $RESULTSAVG "ms to "$TARGET", whilst the maximum response time was "$RESULTSMAX >> $FILE
-        fi
-        sleep 5
-done
+            echo "["$DATE"]" "Ping failure to "$TARGET >> $FILE
+            ping -c 1 $ROUTER &> /dev/null
+            if [[ $? -ne 0 ]]; then
+                echo >> $FILE
+                echo "Ping failure to "$ROUTER "Did it restart?" >> $FILE
+                    else
+                    echo "Connection to Router is alive. Internet appears to be down." >> $FILE
+            fi
+    fi
+    RESULTS=$(ping -c 5 1.1.1.1 | tail -1)
+    RESULTSAVG=$(echo $RESULTS | awk -F '/' '{print $5}' | cut -d "." -f 1 )
+    RESULTSMAX=$(echo $RESULTS | awk -F '/' '{print $6}' | cut -d "." -f 1 )
+if [[ $RESULTSMAX -gt 75 ]]; then
+    echo >> $FILE
+    echo "["$DATE"]" "Ping is reaching high values. The average response time was" $RESULTSAVG "ms to "$TARGET">        
+    fi
